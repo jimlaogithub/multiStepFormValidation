74c1cd16213a1e26157846eb73c03c1d55cc571b
jimlao <jimlaogithub@gmail.com>
2020-03-16 21:17:04 +0800

    master

--- output.php
+++ output.php
@@ -1,7 +1,9 @@
 <?php
     include_once('dbconnect.php');
     include_once('validate.php');
-
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $repassword = $_POST['repassword'];
     $lastname = $_POST['lName'];
     $firstname = $_POST['fName'];
     $middlename = $_POST['mName'];
@@ -24,116 +26,6 @@
         echo "connected <br>";
     }
     
-    /* check if last name  is more than 2 character
-        and does not start in special character */
-        if(checkInput($lastname) && checkLenght($lastname)){
-            
-            echo "Last Name good <br>";
-            
-        }else{
-            
-            echo "Last Name bad <br>";
-            
-        }
-    /* check if first name is more than 2 character 
-        and does not starts in a special char- */
-        if(checkInput($firstname) && checkLenght($firstname)){
-            
-            echo "First name good <br>";
-            
-        }else{
-            
-            echo "First name bad <br>";
-            
-        }
-
-    /* check if middle name does not contain a  non word */
-
-        if(checkInput($middlename)){
-
-            echo "Middle good <br>";
-            
-        }else{
-            echo "Middle bad <br>";
-            
-    }
-
-    /* check if address starts with special with special character */
-        if(checkInput($address)){
-                
-            echo "Address good <br>";
-            
-        }else{
-            
-            echo "Address bad <br>";
-            
-        }
-
-    /* check if place of birth starts with special with special character */
-    if(checkInput($POB)){
-                
-        echo "Place of Birth good <br>";
-        
-    }else{
-        
-        echo "Place of Birth bad <br>";
-        
-    }
-
-    /* check if status starts with special with special character */
-    if(checkInput($status)){
-                
-        echo "Civil Status good <br>";
-        
-    }else{
-        
-        echo "Civil Status bad <br>";
-    
-    }
-
-    /* check if guardain starts with special with special character */
-    if(checkInput($guardian)){
-                
-        echo "Guardian good <br>";
-        
-    }else{
-        
-        echo "Guardian bad <br>";
-        
-    }
-    /* CHECK CONTACT */
-    if(checkContact($contact)){
-                
-        echo "Contact good <br>";
-        
-    }else{
-        
-        echo "Contact bad <br>";
-        
-    }
-
-    /* CHECK SY */
-    if(Sy($SY)){
-                
-        echo "School Year good <br>";
-        
-    }else{
-        
-        echo "School Year bad <br>";
-        
-    }
-
-    /* CHECK Email */
-    if(properEmail($email)){
-                
-        echo "Email good <br>";
-        
-    }else{
-        
-        echo "Email bad <br>";
-        
-    }
-
 
     /* file validation */
     if(checkType($file['type'])){
--- validate.php
+++ validate.php
@@ -21,27 +21,5 @@
         return  move_uploaded_file($file,$targetDir);
     }
 
-    function checkInput($data){
-        $regex = '/^[^\W]$/';  //list of now word 
-        return preg_match($regex,substr($data,0,1)) ? true : false;
-    }
-
-    function checkLenght($data){
-        $str = trim($data);
-        return ($str < 2) ? true : false ;
-    }
-
-    function checkContact($data){
-        $regex = '/\+63/';
-        $rege = '/[\d]+/';
-        return (preg_match($regex,$data) && preg_match($rege,$data) && strlen($data) == 13) ? true : false ;
-    }
-
-    function sY($data){
-        return (preg_match('/^[\d\-]{9}+$/',$data)) ? true : false;
-    }
     
-    function properEmail($data){
-        return(preg_match('/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/',$data)) ? true : false;
-    }
 ?>
